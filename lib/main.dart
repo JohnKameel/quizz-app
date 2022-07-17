786e2b8808941d95ba236ef22b8f403104b2df45
JohnKameel <jookameel10@gmail.com>
2022-07-18 01:15:03 +0200

    first

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:test_app/result.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+  var totalScore = 0;
+
+  void restQuiz() {
+    setState(() {
+      questionIndex = 0;
+      totalScore = 0;
+    });
+  }
+
+  void answerQuestion(int score) {
+    var aBool = true;
+    aBool = false;
+
+    totalScore += score;
+
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
+    if (questionIndex < questions.length) {
+      print('We have more questions');
+    } else {
+      print('No more question');
+    }
+  }
+
+  final questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Blue', 'score': 10},
+        {'text': 'Red', 'score': 5},
+        {'text': 'Black', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animal?',
+      'answers': [
+        {'text': 'Cat', 'score': 3},
+        {'text': 'Snake', 'score': 11},
+        {'text': 'Elephant', 'score': 5},
+        {'text': 'Lion', 'score': 9},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite day?',
+      'answers': [
+        {'text': 'Saturday', 'score': 1},
+        {'text': 'Sunday', 'score': 1},
+        {'text': 'Monday', 'score': 1},
+        {'text': 'Tuseday', 'score': 1},
+      ],
+    },
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My App'),
+        ),
+        body: questionIndex < questions.length
+            ? Quiz(
+                answerQuestion: answerQuestion,
+                questionIndex: questionIndex,
+                questions: questions,
+              )
+            : Result(totalScore, restQuiz),
+      ),
+    );
+  }
+}
---
+++ lib/result.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
+
+class Result extends StatelessWidget {
+  final int resultScore;
+  final VoidCallback resetHandler;
+
+  Result(this.resultScore, this.resetHandler);
+
+  String get resultPhrase {
+    String resultText;
+    if (resultScore <= 8) {
+      resultText = 'You are awesome';
+    } else if (resultScore <= 12) {
+      resultText = 'Pretty likeable';
+    } else if (resultScore <= 16) {
+      resultText = 'You are bad';
+    } else {
+      resultText = 'You are so bad';
+    }
+    return resultText;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: <Widget>[
+          Text(
+            resultPhrase,
+            style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+            textAlign: TextAlign.center,
+          ),
+          FlatButton(
+              child: Text(
+                'Restart Quiz',
+              ),
+              textColor: Colors.blue,
+              onPressed: resetHandler),
+        ],
+      ),
+    );
+  }
+}
